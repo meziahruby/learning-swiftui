8efde426565540622dbf1b160fdad4d1dcacc803
Meziah Cristobal <meziahcristobal@gmail.com>
2023-03-04 17:52:41 -0800

    Initial Commit

---
+++ SolarGram/SolarGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarGramApp.swift
+//  SolarGram
+//
+//  Created by Meziah Cristobal on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SolarGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
